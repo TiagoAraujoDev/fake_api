ecd01a12f6fdacc3268750d45f8b8e43cb14e17d
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-10-17 12:33:45 -0300

    create /items

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import { items } from "./db";
+
+type Items = typeof items;
 
 const fastify = Fastify({
-  logger: true
+  logger: true,
 });
 
-fastify.get("/", (_: FastifyRequest, reply: FastifyReply) => {
-  return reply.send("Hello, world!");
+fastify.get("/items", (_: FastifyRequest, reply: FastifyReply) => {
+  const itemsList: Items = items;
+  return reply.send(itemsList);
 });
 
-fastify.listen({
-  port: 8080,
-  host: "0.0.0.0"
-}, () => {
-  console.log("Server running");
-});
+fastify.listen(
+  {
+    port: 8080,
+    host: "0.0.0.0",
+  },
+  () => {
+    console.log("Server running");
+  }
+);
